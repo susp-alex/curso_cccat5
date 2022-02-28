28f5c8b5a941771e504286e93a1885e9a5fddc0c
Alex Rodrigues <alferodrigues@gmail.com>
2022-02-28 12:01:21 -0300

    Incluindo caso de uso PlaceOrder

---
+++ src/domain/entity/Order.ts
@@ -0,0 +1,41 @@
+import Coupon from "./Coupon";
+import Cpf from "./Cpf";
+import Freight from "./Freight";
+import Item from "./Item";
+import OrderItem from "./OrderItem";
+
+export default class Order {
+	cpf: Cpf;
+	orderItems: OrderItem[];
+	coupon: Coupon | undefined;
+	freight: Freight;
+
+	constructor (cpf: string, readonly issueDate: Date = new Date()) {
+		this.cpf = new Cpf(cpf);
+		this.orderItems = [];
+		this.freight = new Freight();
+	}
+
+	addItem (item: Item, quantity: number) {
+		this.freight.addItem(item, quantity);
+		this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
+	}
+
+	addCoupon (coupon: Coupon) {
+		if (!coupon.isExpired(this.issueDate)) {
+			this.coupon = coupon;
+		}
+	}
+
+	getTotal () {
+		let total = 0;
+		for (const orderItem of this.orderItems) {
+			total += orderItem.getTotal();
+		}
+		if (this.coupon) {
+			total -= this.coupon.calculateDiscount(total);
+		}
+		total += this.freight.getTotal();
+		return total;
+	}
+}
