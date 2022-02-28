31bccb94872cf91e96a703219acdbbd15d688df4
Alex Rodrigues <alferodrigues@gmail.com>
2022-02-28 12:00:34 -0300

    Incluindo caso de uso PlaceOrder

--- src/Dimension.ts
+++
@@ -1,9 +0,0 @@
-export default class Dimension {
-
-	constructor (readonly width: number, readonly height: number, readonly length: number) {
-	}
-
-	getVolume () {
-		return this.width/100 * this.height/100 * this.length/100;
-	}
-}
